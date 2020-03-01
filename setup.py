d52bf3e3b9b412625db20ae48036acb8cf2dca03
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2020-03-01 11:54:59 -0800

    Alpha release

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'dsbox',
+    packages = ['dsbox'],
+    version = 'alpha',  # Ideally should be same as your GitHub release tag varsion
+    description = '',
+    author = 'Kedar Dabhadkar',
+    author_email = 'https://www.linkedin.com/in/dkedar7/',
+    url = 'https://github.com/dkedar7/dsbox',
+    download_url = 'https://github.com/dkedar7/dsbox/archive/alpha.tar.gz',
+    keywords = ['Data Science', 'Machine Learning'],
+    classifiers = [],
+)
