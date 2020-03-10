0456a2285b2450d810d0ffc9b0c8602457921ab2
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2020-03-09 21:55:39 -0700

    Update dependencies

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'dsbox',
     packages = ['dsbox'],
-    version = 'v0.0-alpha',  # Ideally should be same as your GitHub release tag varsion
+    version = 'v0.1-alpha',  # Ideally should be same as your GitHub release tag varsion
     description = '',
     author = 'Kedar Dabhadkar',
     author_email = 'kdabhadk@gmail.com',
@@ -11,4 +11,11 @@ setup(
     download_url = 'https://github.com/dkedar7/dsbox/archive/v0.0-alpha.tar.gz',
     keywords = ['Data Science', 'Machine Learning'],
     classifiers = [],
+    requires_install = ['scipy',
+     'numpy', 
+     'math', 
+     'pandas', 
+     'matplotlib', 
+     'seaborn', 
+     'pandas_profiler']
 )
