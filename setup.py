be5192f99b00ec719a97aecca9892ef9120bb4f5
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2020-03-09 21:12:15 -0700

    Modify setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'dsbox',
     packages = ['dsbox'],
-    version = 'alpha',  # Ideally should be same as your GitHub release tag varsion
+    version = 'v0.0-alpha',  # Ideally should be same as your GitHub release tag varsion
     description = '',
     author = 'Kedar Dabhadkar',
-    author_email = 'https://www.linkedin.com/in/dkedar7/',
+    author_email = 'kdabhadk@gmail.com',
     url = 'https://github.com/dkedar7/dsbox',
-    download_url = 'https://github.com/dkedar7/dsbox/archive/alpha.tar.gz',
+    download_url = 'https://github.com/dkedar7/dsbox/archive/v0.0-alpha.tar.gz',
     keywords = ['Data Science', 'Machine Learning'],
     classifiers = [],
 )
