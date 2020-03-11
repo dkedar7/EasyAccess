8edc0dcdf9a70396e5879ad53502f946d49718ba
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2020-03-10 20:06:16 -0700

    Alpha 0.2

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name = 'dsbox',
     packages = ['dsbox'],
-    version = 'v0.1-alpha',  # Ideally should be same as your GitHub release tag varsion
+    version = 'v0.2-alpha',  # Ideally should be same as your GitHub release tag varsion
     description = '',
     author = 'Kedar Dabhadkar',
     author_email = 'kdabhadk@gmail.com',
     url = 'https://github.com/dkedar7/dsbox',
-    download_url = 'https://github.com/dkedar7/dsbox/archive/v0.1-alpha.tar.gz',
+    download_url = 'https://github.com/dkedar7/dsbox/archive/v0.2-alpha.tar.gz',
     keywords = ['Data Science', 'Machine Learning'],
     classifiers = [],
     install_requires = ['scipy',
