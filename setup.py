0cea903e4ca0a68d5d00d32fbff28a7aca8dfc54
Kedar Dabhadkar <kedar.dabhadkar@gmail.com>
2020-03-16 22:29:40 -0700

    Run test

--- setup.py
+++ setup.py
@@ -1,20 +1,43 @@
 from setuptools import setup
+from pathlib import Path
+
+source_root = Path(".")
 
 setup(
     name = 'dsbox',
     packages = ['dsbox'],
-    version = 'v0.2-alpha',  # Ideally should be same as your GitHub release tag varsion
+    version = 'v0.0.9',  # Ideally should be same as your GitHub release tag varsion
     description = '',
     author = 'Kedar Dabhadkar',
     author_email = 'kdabhadk@gmail.com',
     url = 'https://github.com/dkedar7/dsbox',
-    download_url = 'https://github.com/dkedar7/dsbox/archive/v0.2-alpha.tar.gz',
     keywords = ['Data Science', 'Machine Learning'],
-    classifiers = [],
-    install_requires = ['scipy',
-     'numpy', 
-     'pandas', 
-     'matplotlib', 
-     'seaborn', 
-     'pandas_profiler']
+    python_requires='>=3',
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        "": ["*.txt", "*.rst"]},
+    include_package_data=True,
+    classifiers=[
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Intended Audience :: Healthcare Industry",
+        "Topic :: Scientific/Engineering",
+        "Framework :: IPython",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    install_requires = ['scipy>=1.4.1',
+'pandas==0.25.3',
+'matplotlib==3.2.0',
+'numpy>=1.16.0',
+'tqdm==4.43.0',
+'seaborn',
+'pandas_profiler']
 )
